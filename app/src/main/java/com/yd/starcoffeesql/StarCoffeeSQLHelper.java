4ee85b959f67bbe7f9766be8a22e402a7b429a3d
puz_ya <puzino.y.a@gmail.com>
2017-01-09 22:30:27 +0300

    Changed onCreate, onUpdate to check versions. Strange, but thats all here (new chapter).

--- app/src/main/java/com/yd/starcoffeesql/StarCoffeeSQLHelper.java
+++ app/src/main/java/com/yd/starcoffeesql/StarCoffeeSQLHelper.java
@@ -12,7 +12,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class StarCoffeeSQLHelper extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "starcoffee_db";   //db name
-    public static final int DB_VERSION = 1; //db version
+    public static final int DB_VERSION = 2; //db version
     public static final String TABLE_NAME = "DRINKS";
 
     StarCoffeeSQLHelper(Context context){
@@ -21,16 +21,7 @@ public class StarCoffeeSQLHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db){
-        db.execSQL("CREATE TABLE "+TABLE_NAME+" (" +
-                "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "NAME TEXT, " +
-                "DESCRIPTION TEXT, " +
-                "IMAGE INTEGER); ");
-
-        insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
-        insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
-                R.drawable.cappuccino);
-        insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
+        updateMyDB(db, 0, DB_VERSION);
     }
 
     public void insertDrink(SQLiteDatabase db, String name, String descr, int imageID){
@@ -43,6 +34,24 @@ public class StarCoffeeSQLHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        updateMyDB(db, oldVersion, newVersion);
+    }
 
+    public void updateMyDB(SQLiteDatabase db, int oldVer, int newVer){
+        if(oldVer < 1){
+            db.execSQL("CREATE TABLE "+TABLE_NAME+" (" +
+                    "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "NAME TEXT, " +
+                    "DESCRIPTION TEXT, " +
+                    "IMAGE INTEGER); ");
+
+            insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam", R.drawable.cappuccino);
+            insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
+        }
+
+        if(oldVer < 2){
+            //code
+        }
     }
 }
